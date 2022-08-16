aaabc1d9197ba7a3007f817e47295a5408b76353
grimario-andre <64281623+grimario-andre@users.noreply.github.com>
2022-08-16 00:01:34 -0300

    Create index
    Criadas pastas, paginas header e footer, criando desing delas.

---
+++ app/Includes/header.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title><?= TITLE ?></title>
+</head>
+
+<body class="bg-gradient" style="background-color: #ffffff;">
+    <!-- CONTAINER -->
+    <div class="container">
+        <!-- HEADER -->
+        <nav class="navbar navbar-expand-lg navbar- bg-gradient mt-1 " style="background-color: #f0f0f5;">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <a class="navbar-brand" href="#">Hidden brand</a>
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <!-- END HEADER -->
+
+        <div class="jumbotrom bg-gradient mt-2" style="background-color: #f0f0f5;">
+                <h1 class="text-center">GERADOR DE PEDIDOS E ORDENS DE SERVICO</h1>
+                
+        </div>
+            
+        <!-- MAIN -->
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+//REQUIRE AUTOLOAD
+require_once __DIR__.'/vendor/autoload.php';
+
+//DIFINIR TITULO
+define('TITLE', 'GENPED');
+
+require_once __DIR__.'/app/Includes/header.php';
+require_once __DIR__.'/app/Includes/footer.php';
