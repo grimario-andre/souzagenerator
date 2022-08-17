8d3db9f78ebcff3e57f849a1ade756f1d4a0095a
grimario-andre <64281623+grimario-andre@users.noreply.github.com>
2022-08-17 03:20:00 -0300

    Fix errors
    start to fix header and footer

--- app/Includes/footer.php
+++ app/Includes/footer.php
@@ -1,76 +1,20 @@
- 
- 
- 
+ </div>
+ <!-- END CONTAINER -->
 
-    </div>
-    <!-- END CONTAINER -->
-    <!-- Footer -->
-<footer class="bg-gradient" style="background-color: #ffffff;">
-  <!-- Grid container -->
-  <div class="container p-4">
-    <!-- Section: Form -->
-    <section class="">
-      <form action="">
-        <!--Grid row-->
-        <div class="row d-flex justify-content-center">
-          <!--Grid column-->
-          <div class="col-auto">
-            <p class="pt-2">
-              <strong>Sign up for our newsletter</strong>
-            </p>
-          </div>
-          <!--Grid column-->
 
-          <!--Grid column-->
-          <div class="col-md-5 col-12">
-            <!-- Email input -->
-            <div class="form-outline form-white mb-4">
-              <input type="email" id="form5Example21" class="form-control" />
-              <label class="form-label" for="form5Example21">Email address</label>
-            </div>
-          </div>
-          <!--Grid column-->
+ <!-- SCRIPSTS -->
 
-          <!--Grid column-->
-          <div class="col-auto">
-            <!-- Submit button -->
-            <button type="submit" class="btn btn-outline-light mb-4">
-              Subscribe
-            </button>
-          </div>
-          <!--Grid column-->
-        </div>
-        <!--Grid row-->
-      </form>
-    </section>
-    <!-- Section: Form -->
+ <!-- Option 1: Bootstrap Bundle with Popper -->
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-    <!-- Section: Text -->
-    <section class="mb-4">
-      <p>
-        Projeto de estudos com foco em aprender e melhorar as habilidades Banco de dados, HTML 5, CSS3, Orientacao a Objetos na linguagem de programacao PHP.
-      </p>
-    </section>
-    <!-- Section: Text -->
-
-    <!-- Copyright -->
-  <div class="text-center p-3" style="background-color: #f0f0f5;">
-    © 2022 Copyright:
-    <a class="text-dark" target="_blank" href="https://github.com/grimario-andre/gavagas">GitHub.com/grimario-andre</a>
-  </div>
-  <!-- Copyright -->
-</footer>
-<!-- Footer -->
-    
- <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
+ <!-- Option 2: Separate Popper and Bootstrap JS -->
+ <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     -->
-  </body>
-</html>
+
+   <!-- MOBILE NAV-BAR --> 
+   <script src="/public/mobile.navbar.js"></script>
+ </body>
+
+ </html>
--- index.php
+++ index.php
@@ -6,4 +6,5 @@ require_once __DIR__.'/vendor/autoload.php';
 define('TITLE', 'GENPED');
 
 require_once __DIR__.'/app/Includes/header.php';
+require_once __DIR__.'/app/Includes/login/login.php';
 require_once __DIR__.'/app/Includes/footer.php';
